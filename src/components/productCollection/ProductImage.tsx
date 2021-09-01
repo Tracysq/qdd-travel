43dc3002981fc05fc4a9f48539622cf9fd338532
sunqi <sunqi@xiao100.com>
2021-09-01 20:53:16 +0800

    perf: 去掉无用依赖

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -12,7 +12,7 @@ interface PropsType {
 export const ProductImage:React.FC<PropsType> = ({id, size, title, imageSrc, price}) => {
   return (
     <div>
-      {size == 'large' ? (
+      {size === 'large' ? (
         <Image src={imageSrc} height={285} width={490} />
       ) : (
         <Image src={imageSrc} height={120} width={240} />
